c84fa6c5f4e5936099ccb6f2fe1651cfc5c3428a
Eugenia Valls <e.vallsalfaro@gmail.com>
2023-04-25 18:59:01 +0200

    cambios 25-04

--- src/js/index.js
+++ src/js/index.js
@@ -1,2 +1,26 @@
 // El styles lo importamos aquí, ya se carga después al compilar todo
 import '../scss/styles.scss';
+
+const darkMode = document.getElementById ('dark-mode');
+const icon = document.getElementById ('icon');
+const headerText = document.getElementById ('icon-text');
+const logo = document.getElementById ('logo');
+
+
+function handleClick (){
+    if (icon.classList.contains('fa-moon')){
+        icon.classList.replace('fa-moon','fa-sun');
+        headerText.textContent= 'Light mode';
+        logo.src="./assets/images/logo.svg";
+        
+    } else if (icon.classList.contains('fa-sun')){
+        icon.classList.replace('fa-sun','fa-moon');
+        headerText.textContent= 'Dark mode';
+        logo.src="./assets/images/logo-dark.svg";
+    }
+    document.body.classList.toggle('light');
+
+
+}
+
+darkMode.addEventListener('click',handleClick);
